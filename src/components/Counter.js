54d49258f82b6b86b3d0454e48443ed5ace22cfe
ngxpaul <phongnx.22git@vku.udn.vn>
2024-03-05 13:33:33 +0700

    toggle Button works

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,7 +5,10 @@ import classes from './Counter.module.css';
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
-  const toggleCounterHandler = () => {};
+  const show=useSelector(state => state.showCounter)
+  const toggleCounterHandler = () => {
+    dispatch({type: 'toggle'});
+  };
   const incrementHandler = () => {
     dispatch({type: 'increment'});
   }
@@ -19,7 +22,7 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={decrementHandler}>Decrement</button>
         <button onClick={increaseHandler}>Increase by 5</button>
